ca905e54ec0a4f6dbdadc6bd897977a37fe1b966
Mark Suckerberg <mark@suckerberg.ga>
2021-12-05 14:21:05 -0600

    makes traits fetched at release rather than stored in the repo

---
+++ .github/get-traits.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+import requests
+import json
+import re
+
+page = requests.get('https://cosmosdex.com/cosmosdex/traits/')
+soup = BeautifulSoup(page.text, 'html.parser')
+
+list_elems = soup.find_all("div", class_="dexsectiontextmiddle")
+list_traits = []
+
+for elem in list_elems:
+    result = {}
+    if elem.find(["div", "a"], class_="bluetrait"):
+        result["value"] = 0
+    elif elem.find(["div", "a"], class_="greentrait"):
+        result["value"] = 1
+    elif elem.find(["div", "a"], class_="redtrait"):
+        result["value"] = 2
+    elif elem.find(["div", "a"], class_="purpletrait"):
+        result["value"] = 3
+    elif elem.find(["div", "a"], class_="yellowtrait"):
+        result["value"] = 4
+    else:
+        print(f"error! {elem}")
+
+    result["name"] = elem.find("a").string.strip("[]")
+    result["effects"] = elem.find("p").string or re.sub('<[^<]+?>', '', str(elem.find("p")))
+
+    list_traits.append(result)
+
+list_traits.sort(key=lambda word:word["name"])
+
+with open("traits.json", "w") as write_file:
+    json.dump(list_traits, write_file)
